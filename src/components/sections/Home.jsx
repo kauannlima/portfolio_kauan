97f1da1c29e78674904bf3399d1c6c011f1288c6
kauannlima <124539523+kauannlima@users.noreply.github.com>
2025-07-04 12:07:28 -0300

    Atualizar o Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -18,30 +18,32 @@ export const Home = () => {
             desenvolvimento de soluções práticas que me permitem evoluir
             tecnicamente e construir um portfólio sólido.
           </p>
-          <div className="flex justify-center space-x-4">
-            <a
-              href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
-            >
-              Ver projetos
-            </a>
-            <a
-              href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50"
-            >
-              Entre em contato ou Fale comigo
-            </a>
-            <a
-              href="/Kauan-Lima-Curriculo-Dev-BackEnd.pdf"
-              download
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50"
-            >
-              Baixar Currículo
-            </a>
-          </div>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+  <a
+    href="#projects"
+    className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
+    hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] text-center w-full md:w-auto"
+  >
+    Ver projetos
+  </a>
+
+  <a
+    href="#contact"
+    className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+    hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50 text-center w-full md:w-auto"
+  >
+    Fale comigo
+  </a>
+
+  <a
+    href="/Kauan-Lima-Curriculo-Dev-BackEnd.pdf"
+    download
+    className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+    hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50 text-center w-full md:w-auto"
+  >
+    Baixar Currículo
+  </a>
+</div>
         </div>
       </RevealOnScroll>
     </section>
