aca3d672bfadfacb995e70dae42a52deaef4f8ca
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-07-03 23:55:08 -0300

    Primeiro testes no envio do e-mail

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 mb-6 bg-clip-text text-transparent leading-right ">
-            Olá, eu sou o Kauan Lima
+            Olá, Sou Kauan Lima
           </h1>
 
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
@@ -24,14 +24,22 @@ export const Home = () => {
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
-              Ver Projetos
+              Ver projetos
             </a>
             <a
               href="#contact"
               className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50"
             >
-              Contate-me
+              Entre em contato ou Fale comigo
+            </a>
+            <a
+              href="/Kauan-Lima-Curriculo-Dev-BackEnd.pdf"
+              download
+              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/50"
+            >
+              Baixar Currículo
             </a>
           </div>
         </div>
