7e3f5eda0b5e08e85cad002785f4aae1b97acc0d
kauannlima <124539523+kauannlima@users.noreply.github.com>
2025-07-04 14:17:50 -0300

    Atualizar o Contact.jsx

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -32,7 +32,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className=" w-150 px-4">
+        <div className="w-[200px] sm:w-[300px] md:w-[400px] px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             Entre em contato
           </h2>
