037b14d81b3d5f0d4a96f8789a8c5fc3d97b6dc8
kauannlima <kauanalmeidalima1405@gmail.com>
2026-07-02 20:41:52 -0300

    Remove unnecessary whitespace in Contact component

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -175,7 +175,6 @@ export const Contact = () => {
                           </p>
                         </div>
                       </div>
-                      
                     </a>
                   );
                 })}
